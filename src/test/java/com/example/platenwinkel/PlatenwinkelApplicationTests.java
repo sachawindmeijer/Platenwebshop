d60d3b9817623926b796af3eb190a673f8378ad5
sachawindmeijer <sachawindmeijer@hotmail.com>
2024-05-30 18:04:13 +0200

    Basis gelegd voor database

---
+++ src/test/java/com/example/platenwinkel/PlatenwinkelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platenwinkel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatenwinkelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
